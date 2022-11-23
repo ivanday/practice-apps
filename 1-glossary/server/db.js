c8958d0873cacb6b1b4cf1718994c861d45ce360
Ivan Day <ivanday@utexas.edu>
2022-11-23 10:41:44 -0600

    Setup basics of server and configure npm

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -4,3 +4,5 @@ const mongoose = require("mongoose");
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+
+
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -4,16 +4,25 @@ const path = require("path");
 
 const app = express();
 
+const PORT = process.env.PORT || 4000;
+
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, '../client/src')));
 
-/**** 
- * 
- * 
+
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
-app.listen(process.env.PORT);
-console.log(`Listening at http://localhost:${process.env.PORT}`);
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${PORT}`);
+});
+console.log(`Listening at http://localhost:${PORT}`);
