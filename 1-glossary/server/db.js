6b4b3c755c6c46f59c7739d13078d7f2fe78e86d
Ivan Day <ivanday@utexas.edu>
2022-11-23 11:04:06 -0600

    Add framework of database

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,8 +1,26 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/fetcher');
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
 
+const word = new Schema ({
+  name: String,
+  definition: String
+});
 
+const Word = mongoose.model('Word', word);
+
+let save = function(data) {
+
+};
+
+let get = function() {
+
+};
+
+let update = function() {
+
+};
